7b6189f43e91121e29734a1c35a2a59ef876a969
Armando Bretas <armando.luiz@gazin.com.br>
2020-05-18 02:24:27 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+}); // This will emit the event to all connected sockets
+io.on("connection", (socket) => {
+  socket.broadcast.emit("hi");
+});
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
